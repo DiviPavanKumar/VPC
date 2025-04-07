6a87f7fb77c8da7dca9d1c41e7cb5d40425c2b4e
divipavankumar92 <147476470+divipavankumar92@users.noreply.github.com>
2025-04-08 02:44:53 +0530

    Adding Files

--- vpc_demo.tf
+++ vpc_demo.tf
@@ -4,3 +4,55 @@ resource "aws_vpc" "vpc_demo" {
     Name = "VPN_Demo_Testing"
   }
 }
+
+resource "aws_subnet" "vpc_demo_subnets" {
+
+  for_each = { for subnet in var.subnet_configs : subnet.name => subnet }
+
+  vpc_id            = aws_vpc.vpc_demo.id
+  cidr_block        = each.value.cidr_block
+  availability_zone = each.value.availability_zone
+  tags = {
+    Name = each.key
+  }
+}
+
+resource "aws_route_table" "public" {
+  vpc_id = aws_vpc.vpc_demo.id
+
+  tags = {
+    Name = "public-route-table"
+  }
+}
+
+resource "aws_route_table" "private" {
+  vpc_id = aws_vpc.vpc_demo.id
+
+  tags = {
+    Name = "private-route-table"
+  }
+}
+
+resource "aws_internet_gateway" "igw" {
+  vpc_id = aws_vpc.vpc_demo.id
+  tags = {
+    Name = "IGW"
+  }
+}
+
+resource "aws_route" "public_internet_access" {
+  route_table_id         = aws_route_table.public.id
+  destination_cidr_block = "0.0.0.0/0"
+  gateway_id             = aws_internet_gateway.igw.id
+}
+
+resource "aws_route_table_association" "subnet_associations" {
+  for_each = { for subnet in var.subnet_configs : subnet.name => subnet }
+
+  subnet_id = aws_subnet.vpc_demo_subnets[each.key].id
+
+  route_table_id = each.value.name == "Web" ? aws_route_table.public.id : aws_route_table.private.id
+}
+
+
+
